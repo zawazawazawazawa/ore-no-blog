16c805f02d76d31adf6d7fcd8be7a6289029dc99
zawazawazawazawa <bosse.de.poulet@gmail.com>
2018-12-16 13:02:07 +0900

    Make index, show, new, create, edit, update and destroy controller

---
+++ app/controllers/diaries_controller.rb
@@ -0,0 +1,47 @@
+class DiariesController < ApplicationController
+  def index
+    @diaries = Diary.all
+  end
+
+  def show
+    @diary = Diary.find(params[:id])
+  end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to @diary
+    else
+      render new
+    end
+  end
+
+  def edit
+    @diary = Diary.find(params[:id])
+  end
+
+  def update
+    @diary = Diary.find(params[:id])
+    if @diary.update(diary_params)
+      redirect_to @diary
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to diaries_url, notice: "Diary was successfully destroyed."
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:title, :body)
+  end
+
+end
